c3a1a0ddf1d19e9f7efbbf21e4c6c57f0dd2e16d
Matthew <matthew.bowman@tutanota.com>
2026-03-01 15:00:05 +0000

    Seperated screens from routing

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,17 +1,1 @@
-import { Text, useColorScheme } from 'react-native';
-import { useTheme } from '@/theme/ThemeProvider';
-
-export default function Home() {
-
-  const sysTheme = useColorScheme()
-  const { theme, preference, isLoading, colors } = useTheme();
-
-  return (
-    <>
-      <Text style={{ color: colors.mainText }}>Theme - {theme}</Text>
-      <Text style={{ color: colors.mainText }}>Preference - {preference}</Text>
-      <Text style={{ color: colors.mainText }}>Is Loading - {isLoading ? 'True' : 'False'}</Text>
-      <Text style={{ color: colors.mainText }}>System Theme - {sysTheme}</Text>
-    </>
-  );
-}
+export { default } from "@/screens/HomeScreen";
--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -1,9 +1,1 @@
-import { View, Text } from 'react-native';
-
-export default function Settings() {
-  return (
-    <View>
-      <Text>Settings Screen</Text>
-    </View>
-  );
-}
+export { default } from "@/screens/SettingsScreen";
