7426c3090ab6b914da28a4780ebdb502162ae3d7
Matthew <matthew.bowman@tutanota.com>
2026-03-01 21:18:50 +0000

    Changed size of ActivityIndicator

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -47,7 +47,7 @@ export default function Button({
             accessibilityState={{ disabled: isDisabled, busy: !!loading }}
         >
             {loading ? (
-                <ActivityIndicator color={colors.onAccent} />
+                <ActivityIndicator size={18} color={colors.onAccent} />
             ) : (
                 <Text variant="subheading" style={{
                     color: colors.onAccent,
